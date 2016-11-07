f0206a2dcb15bf8aa41be15d87d20e4e72ae9572
Bradley Coudriet <bjcpgd@rit.edu>
2016-11-07 09:28:05 -0500

    Disabled config menu for now

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Proxmox-Dashboard
 A Laravel Application that manages our Proxmox Cluster. Features include cluster resources and a feature that balances QEMU guests across the cluster.
+
+
--- resources/views/menu/nav.blade.php
+++ resources/views/menu/nav.blade.php
@@ -3,4 +3,4 @@
 <li {{ (Request::is('/') ? 'class=active' : '') }}><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard <span class="sr-only">(current)</span></a></li>
 <li {{ (Request::is('provision') ? 'class=active' : '') }}><a href="{{ route('provision') }}"><i class="fa fa-desktop"></i> Provision</a></li>
 <li {{ (Request::is('tasks') ? 'class=active' : '') }}><a href="{{ route('tasks') }}"><i class="fa fa-list"></i> Tasks</a></li>
-<li {{ (Request::is('config') ? 'class=active' : '') }}><a href="{{ route('config') }}"><i class="fa fa-gears"></i> Configuration</a></li>
+<!--li {{ (Request::is('config') ? 'class=active' : '') }}><a href="{{ route('config') }}"><i class="fa fa-gears"></i> Configuration</a></li-->
