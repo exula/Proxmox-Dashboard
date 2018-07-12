58f4d638a0c873ec11bd19e4c0c0c7db553f12c4
Bradley Coudriet <bjcpgd@rit.edu>
2018-07-12 14:29:08 -0400

    Make tests run

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/dash');
 
         $response->assertStatus(200);
     }
