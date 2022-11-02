8ebbebc0b7104a6b60247bea1d0aff008dc20ced
Shift <shift@laravelshift.com>
2022-11-02 18:48:34 +0000

    Adopt class based routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MapController;
+use App\Http\Controllers\ProvisionController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,22 +16,22 @@
 |
 */
 
-Route::get('/', 'HomeController@index')->name('dashboard')->middleware('proxmoxauth');
+Route::get('/', [HomeController::class, 'index'])->name('dashboard')->middleware('proxmoxauth');
 
-Route::get('/dash', 'HomeController@dash')->name('dash');
+Route::get('/dash', [HomeController::class, 'dash'])->name('dash');
 
-Route::get('/dashboardData', 'HomeController@dashboardData')->name('dashboardData');
-Route::post('dorecommendations', 'HomeController@doRecommendations')->name('dorecommendations')->middleware('proxmoxauth');
-Route::get('history', 'HomeController@history')->name('history')->middleware('proxmoxauth');
-Route::get('tasks', 'HomeController@tasks')->name('tasks')->middleware('proxmoxauth');
-Route::get('provision', 'ProvisionController@create')->name('provision')->middleware('proxmoxauth');
-Route::post('provision', 'ProvisionController@store')->name('doProvision')->middleware('proxmoxauth');
-Route::get('map', 'MapController@index')->name('map')->middleware('proxmoxauth');
-Route::post('map/dorecommendations', 'MapController@doRecommendations')->name('map/dorecommendations')->middleware('proxmoxauth');
+Route::get('/dashboardData', [HomeController::class, 'dashboardData'])->name('dashboardData');
+Route::post('dorecommendations', [HomeController::class, 'doRecommendations'])->name('dorecommendations')->middleware('proxmoxauth');
+Route::get('history', [HomeController::class, 'history'])->name('history')->middleware('proxmoxauth');
+Route::get('tasks', [HomeController::class, 'tasks'])->name('tasks')->middleware('proxmoxauth');
+Route::get('provision', [ProvisionController::class, 'create'])->name('provision')->middleware('proxmoxauth');
+Route::post('provision', [ProvisionController::class, 'store'])->name('doProvision')->middleware('proxmoxauth');
+Route::get('map', [MapController::class, 'index'])->name('map')->middleware('proxmoxauth');
+Route::post('map/dorecommendations', [MapController::class, 'doRecommendations'])->name('map/dorecommendations')->middleware('proxmoxauth');
 
-Route::get('virtualmachines', 'HomeController@virtualmachines')->name('virtualmachines')->middleware('proxmoxauth');
+Route::get('virtualmachines', [HomeController::class, 'virtualmachines'])->name('virtualmachines')->middleware('proxmoxauth');
 
-Route::get('config', 'HomeController@config')->name('config')->middleware('proxmoxauth');
+Route::get('config', [HomeController::class, 'config'])->name('config')->middleware('proxmoxauth');
 
 Route::post('/deploymentHook', function () {
     Log::info('Resetting opcache for '.php_sapi_name());
